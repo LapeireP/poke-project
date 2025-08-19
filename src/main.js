e1493e1beaccfae3f789d2211c22bb2c7b501c25
LapeireP <par_lapeire@hotmail.com>
2025-08-19 20:53:50 +0200

    Kleine wijziging HTML + functie weergeven Pkmn
    - Voorbeeldlijst verwijderd in HTML
    - Standard Vite-code verwijderd in main.js
    - Functie toegevoegd om Pokémon weer te geven met id, naam en afbeelding
    - CSS aangepast om overzichtelijk te blijven en om de data van de Pokémon in een mooi blokje te zetten

--- src/main.js
+++ src/main.js
@@ -1,28 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import { fetchPokemonData } from '/api/pokeapi.js';
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
 
 //Testje of de data in de console wordt gezet
 async function testFetchData() {
@@ -34,3 +11,36 @@ async function testFetchData() {
   }
 }
 testFetchData();
+
+async function geefPokemonWeer() {
+  const pokemonLijst = await fetchPokemonData();
+  const pokemonLijstContainer = document.getElementById('pokemon-lijst');
+  
+  // Data ophalen
+  const pokemonDetailsPromises = pokemonLijst.map(async (pokemon) => {
+    const response = await fetch(pokemon.url);
+    const data = await response.json();
+    return {
+      id: data.id,
+      name: data.name,
+      sprite: data.sprites.front_default,
+    };
+  });
+
+  // Promise
+  const pokemonDetails = await Promise.all(pokemonDetailsPromises);
+
+  // Aanmaken pokemon-kaarten
+  pokemonLijstContainer.innerHTML = pokemonDetails.map(pokemon => `
+    <div class="pokemon-kaart">
+    <div class="pokemon-kaartinfo">
+    <button class="favorietenknop">♥</button>
+      <h3>${pokemon.id}</h3>
+      <h3>${pokemon.name}</h3>
+      <img src="${pokemon.sprite}" alt="${pokemon.name}">
+      </div>
+    </div>
+  `).join('');
+}
+
+geefPokemonWeer();
